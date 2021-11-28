31bc95a84edf3b7b8bc881324392fffa2521aed0
MichalCipa <michalcipa@gmail.com>
2021-11-28 18:03:16 +0100

    refactor: use autoImports alias system

--- module.mjs
+++ module.mjs
@@ -1,5 +1,4 @@
 import { defineNuxtModule } from "@nuxt/kit";
-import fs from "fs";
 import lodash from "lodash-es";
 
 export default defineNuxtModule({
@@ -7,23 +6,14 @@ export default defineNuxtModule({
   defaults: { prefix: false, exclude: [] },
   setup(options, nuxt) {
     const prefix = options.prefix || "";
-    const exportsArray = [];
-    const typesArray = [];
-    const names = [];
+    const autoImportNames = [];
 
     for (const [key] of Object.entries(lodash)) {
       if (!options.exclude.includes(key)) {
         const name = prefix ? lodash.upperFirst(key) : key;
-        exportsArray.push(`export const ${prefix}${name} = lodash.${key}`);
-        typesArray.push(`export const ${prefix}${name}: typeof import('nuxt-lodash/functions')['${key}']`);
-        names.push(prefix + name);
+        autoImportNames.push({ name: key, as: prefix + name });
       }
     }
-    const exportString = exportsArray.join("\n");
-    const typesString = typesArray.join("\n");
-    fs.writeFileSync("node_modules/nuxt-lodash/functions.mjs", `import lodash from 'lodash-es';\n\n${exportString}`);
-    fs.writeFileSync("node_modules/nuxt-lodash/functions.d.ts", typesString);
-
     nuxt.hook("vite:extend", ({ config }) => {
       config.optimizeDeps ||= {};
       config.optimizeDeps.exclude ||= [];
@@ -32,8 +22,8 @@ export default defineNuxtModule({
 
     nuxt.hook("autoImports:sources", (sources) => {
       sources.push({
-        from: "nuxt-lodash/functions",
-        names,
+        from: "lodash-es",
+        names: autoImportNames,
       });
     });
   },
--- types.d.ts
+++ types.d.ts
@@ -1,27 +1,27 @@
 declare type NuxtLodashOptions = {
-    /**
-     * Prefix to be added before every lodash function.
-     * false to disable prefix
-     */
-    prefix?: false | string;
-    /**
-     * Array of lodash funcions to be exluded from auto-imports
-     */
-    exludes?: string[];
-  };
-  declare module "nuxt3" {
-    interface NuxtConfig {
-      lodash?: NuxtLodashOptions;
-    }
+  /**
+   * Prefix to be added before every lodash function.
+   * false to disable prefix
+   */
+  prefix?: false | string;
+  /**
+   * Array of lodash funcions to be exluded from auto-imports
+   */
+  exlude?: string[];
+};
+declare module "nuxt3" {
+  interface NuxtConfig {
+    lodash?: NuxtLodashOptions;
   }
-  declare module "@nuxt/types" {
-    interface NuxtConfig {
-      lodash?: NuxtLodashOptions;
-    }
+}
+declare module "@nuxt/types" {
+  interface NuxtConfig {
+    lodash?: NuxtLodashOptions;
   }
-  declare module "@nuxt/kit" {
-    interface NuxtConfig {
-      lodash?: NuxtLodashOptions;
-    }
+}
+declare module "@nuxt/kit" {
+  interface NuxtConfig {
+    lodash?: NuxtLodashOptions;
   }
-  export { NuxtLodashOptions };
+}
+export { NuxtLodashOptions };
