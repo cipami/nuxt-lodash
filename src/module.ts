ee83bf607a885792e27e2d7a0d3172966de668f5
daniil4udo <daniil4udo@gmail.com>
2022-03-11 15:04:57 +0400

    fix: duplicate imports on hot reload

--- src/module.ts
+++ src/module.ts
@@ -60,6 +60,9 @@ export default defineNuxtModule<ModuleOptions>({
     })
 
     nuxt.hook('autoImports:sources', (sources) => {
+      if (sources.find(i => i.from === 'lodash-es'))
+          return
+
       sources.push({ names, from: 'lodash-es' })
     })
   }
