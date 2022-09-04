6c087cecb50981c445d3ede536b0dc740e3ffc26
MichalCipa <michalcipa@gmail.com>
2022-09-04 16:41:07 +0200

    feat: upgrade to rc.9

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { addImports, defineNuxtModule } from '@nuxt/kit'
 import * as lodash from 'lodash-es'
 import exculdeDefaults from './exclude'
 
@@ -9,25 +9,25 @@ export interface ModuleOptions {
    *
    * @defaultValue `use`
    */
-  prefix?: false | string;
+  prefix: false | string;
   /**
    * Functions that starts with keywords in this array will be skipped by prefix
 
    * @defaultValue ['is']
    */
-  prefixSkip?: string[];
+  prefixSkip: string[];
   /**
    * Array of lodash funcions to be exluded from auto-imports
    *
    * @defaultValue []
    */
-  exclude?: string[];
+  exclude: string[];
   /**
    * Iterable of string pairs to alias each function
    *
    * @defaultValue []
    */
-  alias?: Iterable<[string, string]>;
+  alias: Iterable<[string, string]>;
 }
 
 export default defineNuxtModule<ModuleOptions>({
@@ -35,7 +35,7 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'nuxt-lodash',
     configKey: 'lodash',
     compatibility: {
-      nuxt: '^3.0.0'
+      nuxt: '^3.0.0-rc.9'
     }
   },
   defaults: {
@@ -45,19 +45,18 @@ export default defineNuxtModule<ModuleOptions>({
     alias: []
   },
   setup (options, nuxt) {
-    const imports = []
     const prefix = options.prefix || ''
-    const aliasMap = new Map(options.alias)
+    const aliasMap = new Map<string, string>(options.alias)
     const exludes = [...options.exclude, ...exculdeDefaults]
 
     for (const [name] of Object.entries(lodash)) {
       if (!exludes.includes(name)) {
-        const alias = aliasMap.has(name) ? aliasMap.get(name) : name
+        const alias = aliasMap.has(name) ? aliasMap.get(name)! : name
         const as = (() => {
           const isPrefix = !options.prefixSkip.some(key => alias.startsWith(key)) && prefix
           return isPrefix ? prefix + lodash.upperFirst(alias) : alias
         })()
-        imports.push({ name, as })
+        addImports({ name, as, from: 'lodash-es' })
       }
     }
 
@@ -65,12 +64,6 @@ export default defineNuxtModule<ModuleOptions>({
       config.optimizeDeps ||= {}
       config.optimizeDeps.exclude ||= []
       config.optimizeDeps.exclude.push('lodash-es')
-    })
-
-    nuxt.hook('autoImports:sources', (sources) => {
-      if (!sources.some(i => i.from === 'lodash-es')) {
-        sources.push({ imports, from: 'lodash-es' })
-      }
     })
   }
 })
